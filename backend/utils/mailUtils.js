06c7ef747a043392d2d6a034a013eca064905086
Rathivarman P <rathivarma2005@gmail.com>
2026-05-05 14:47:22 +0530

    First Commit

--- backend/utils/mailUtils.js
+++ backend/utils/mailUtils.js
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true, // SMTPS
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -14,9 +14,11 @@ const transporter = nodemailer.createTransport({
   tls: {
     rejectUnauthorized: false,
   },
-  connectionTimeout: 10000, // 10 seconds
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  connectionTimeout: 45000, // 45 seconds
+  greetingTimeout: 45000,
+  socketTimeout: 45000,
+  logger: true,
+  debug: true,
 });
 
 export const sendOTPEmail = async (email, otp) => {
