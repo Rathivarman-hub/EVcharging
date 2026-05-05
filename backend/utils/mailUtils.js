d273958875986585168b8da460f0449cd14a1590
Rathivarman P <rathivarma2005@gmail.com>
2026-05-05 14:33:16 +0530

    First Commit

--- backend/utils/mailUtils.js
+++ backend/utils/mailUtils.js
@@ -4,13 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    minVersion: 'TLSv1.2'
   }
 });
 
